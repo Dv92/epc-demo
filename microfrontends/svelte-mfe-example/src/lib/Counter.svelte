4f05eabf0d9efe19574e2e1b9afcaaf6b98832ea
Marco Corradetti <m.corradetti@entando.com>
2022-10-03 17:19:45 +0200

    add mfe svelte

---
+++ microfrontends/svelte-mfe-example/src/lib/Counter.svelte
@@ -0,0 +1,22 @@
+<svelte:options tag="cp-counter" />
+
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment} class="button">
+  count is {count}
+</button>
+
+<style>
+  .button {
+    border: 0px none;
+    background-color: grey;
+    color: white;
+    padding: 16px;
+    border-radius: 5px;
+  }
+</style>
